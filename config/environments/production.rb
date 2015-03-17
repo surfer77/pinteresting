fd47c24f3fb2a485724e82003488861cb2e003e6
Adam Hodara <hodaraadam@gmail.com>
2015-03-17 01:47:09 +0000

    added asw3 for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,14 @@ Rails.application.configure do
   
  config.action_mailer.default_url_options = { host: 'adam-pinterest.herokuapp.com' }
 
+# config/environments/production.rb set pprclick to upload iamges to amazon s3
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 end
